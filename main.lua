cdc0c99a9473391bdac4e7218e41e94ebd4df7de
tharu62 <tharinduxdeshan@gmail.com>
2025-04-21 18:53:55 +0200

    first commit

---
+++ main.lua
@@ -0,0 +1,38 @@
+require "lfs"
+
+-- This script creates a new CMake project directory structure
+
+print("Enter the programming language (c/cpp/py): ")
+exstension = io.read("*l")
+if exstension == "c" then
+    exstension = ".c"
+elseif exstension == "cpp" then
+    exstension = ".cpp"
+elseif exstension == "py" then
+    exstension = ".py"
+else
+    print("Invalid programming language. Please enter c, cpp, or py.")
+    return
+end
+
+print("Enter the name of the project:")
+project_name = io.read("*l") 
+lfs.mkdir(project_name)
+lfs.mkdir(project_name .. "/include")
+lfs.mkdir(project_name .. "/src")
+io.output(project_name .. "/src/main" .. exstension)
+
+if exstension == ".py" then
+    io.close()
+    return 
+end
+
+io.output(project_name .. "/CMakeLists.txt")
+
+-- fill the CMakeLists.txt 
+io.write("cmake_minimum_required(VERSION 3.28)\n")
+io.write("project(" .. project_name .. " LANGUAGES CXX)\n\n\n")
+io.write("include_directories(".. "include" .. ")\n")
+io.write("add_executable(main src/main.cpp)\n")
+io.write("target_compile_features(main PRIVATE cxx_std_17)\n")
+io.close()
